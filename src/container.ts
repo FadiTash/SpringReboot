e3e82ae1c41a572a4abc63660606048e331ed88e
I504920 <gregory.hakomaki.ii@sap.com>
2020-04-19 17:34:52 +0200

    init request

--- src/container.ts
+++ src/container.ts
@@ -1,30 +1,82 @@
 import express = require("express"); 
+import session = require("express-session"); 
+import bodyParser = require("body-parser");
 
+enum RequestMethod {
+    Get = 'get',
+    Post = 'post',
+    Delete = 'delete',
+    Put = 'put',
+}
+
+enum Scope {
+    Application,
+    Session,
+    Request,
+}
+class DuplicateINameError extends Error {
+    public constructor(iName: string){
+        super(`This item ${iName} already exists as an Application scoped object`);
+    }
+}
+class InvalidINameError extends Error {
+    public constructor(iName: string){
+        super(`Session of ${iName} is not available in the container`);
+    }
+}
 class Container {
-    public content: {[key: string]: any} = {};
+    public applicationScope: {[key: string]: any} = {};
+    public sessionScope: {[key: string]: any} = {};
+    public requestScope: {[key: string]: any} = {};
     public constructor() {
 
     }
 
-    public set(iName: string, Ob: any) {
-        if (this.content[iName]) {
-            throw new Error("This item " + iName + " already exists as an Application scoped object");
+    public set(iName: string, Ob: any, scope?: Scope) {
+        let targetMap = this.applicationScope;
+        switch(scope){
+            case Scope.Session:
+                targetMap = this.sessionScope;
+                break;
+            case Scope.Request:
+                targetMap = this.requestScope;
+                break;
+            case Scope.Application:
+            default:
+                break;
         }
-        this.content[iName] = Ob;        
+        if (targetMap[iName]) {
+          throw new DuplicateINameError(iName);
+        }
+        targetMap[iName] = Ob;
     }
 
-    public get(iName: string): any {
-        if (this.content[iName] === undefined) {
-            throw new Error ("Asset " + iName + " could not be aquired");
+    public get(iName: string, scope?: Scope): any {
+        switch (scope) {
+          case Scope.Session:
+            if (this.sessionScope[iName]) {
+              throw new InvalidINameError(iName);
+            }
+            return this.sessionScope[iName];
+          case Scope.Request:
+            if (this.requestScope[iName]) {
+              throw new InvalidINameError(iName);
+            }
+            return new this.requestScope[iName]();
+          case Scope.Application:
+          default:
+            if (this.applicationScope[iName]) {
+              throw new InvalidINameError(iName);
+            }
+            return this.applicationScope[iName];
         }
-        return this.content[iName];
     }
 
     public getAs<T>(iName: string): T {
-        if (this.content[iName] === undefined) {
+        if (this.applicationScope[iName] === undefined) {
             throw new Error ("Asset " + iName + " could not be aquired");
         }
-        return (this.content[iName] as T);
+        return (this.applicationScope[iName] as T);
     } 
 
 }
@@ -39,6 +91,19 @@ export function ApplicationScope(iName?: string) {
     }
 }
 
+// export function SessionScope(iName?: string){
+//     return function(target: any) {
+//         container.set()
+//     }
+// }
+export function RequestScope(iName?: string){
+    return function(target: any) {
+        iName = iName || target.name;
+        iName = iName || "";
+        container.set(iName, target, Scope.Request);
+    }
+}
+
 class Spring {
     public app: express.Application = express();
     private main: ISpringApplication | undefined; // call this configuration instead
@@ -47,6 +112,10 @@ class Spring {
     public setApplication(obj: ISpringApplication) {
         this.main = obj;
         const port = this.main ? this.main.port : 8080;
+        this.app.use(bodyParser.json());
+        if(this.main && this.main.session){
+            this.app.use(session(this.main.session));
+        }
         this.app.listen(port, function() {
             console.log("Application started on port: " + port);
         });
@@ -54,13 +123,6 @@ class Spring {
     public getApplication(): ISpringApplication | undefined {
         return this.main;
     }
-}
-
-enum RequestMethod {
-    Get = 'get',
-    Post = 'post',
-    Delete = 'delete',
-    Put = 'put'
 }
 
 export const springContainer = new Spring();
@@ -95,7 +157,8 @@ export function Post(route: string) {
 
 export interface ISpringApplication {
     port: number;
-    ssl?: {[key: string] : string}
+    ssl?: {[key: string] : string};
+    session?: any;
 }
 
 export function use(target: any) {
